12351d852a55b3b80b45bbe8de9d759aa189d3f8
Avtion <mail@avtion.cn>
2021-07-22 00:08:39 +0800

    feature: 新增gops支持

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"github.com/google/gops/agent"
+	"go.uber.org/zap"
+)
+
+func init() {
+	if err := agent.Listen(agent.Options{}); err != nil {
+		log.Error("gops agent run failed", zap.Error(err))
+		return
+	}
+}
+
 func main() {
 	InitCfg()
 	InitRobots(globalCfg)
