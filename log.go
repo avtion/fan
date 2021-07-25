b1752dc780975dd6c20dfe48fd3e0600c1bba501
Avtion <mail@avtion.cn>
2021-07-25 19:41:23 +0800

    feature: 支持Cron的日志对象

--- log.go
+++ log.go
@@ -1,7 +1,26 @@
 package main
 
 import (
+	"github.com/robfig/cron/v3"
 	"go.uber.org/zap"
 )
 
 var log, _ = zap.NewProduction()
+
+type cronLogger struct {
+	ZapLogger *zap.Logger
+}
+
+var _ cron.Logger = (*cronLogger)(nil)
+
+func (c *cronLogger) Info(msg string, keysAndValues ...interface{}) {
+	c.ZapLogger.Sugar().Infow(msg, keysAndValues...)
+}
+
+func (c *cronLogger) Error(err error, msg string, keysAndValues ...interface{}) {
+	c.ZapLogger.With(zap.Error(err)).Sugar().Errorw(msg, keysAndValues...)
+}
+
+func getCronLogger() *cronLogger {
+	return &cronLogger{ZapLogger: log}
+}
